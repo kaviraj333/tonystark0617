3022598a8d1ae8f9e1d74af0643a9ad844997a15
tonystark0617 <42027695+tonystark0617@users.noreply.github.com>
2018-08-21 11:38:01 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,9 @@
+a=int(input(""))
+b=int(input(""))
+c=int(input(""))
+if ((a) > (b) and a> c):
+    print("largest number" +str(a))
+elif (b > a and b > c):
+    print("largest number" +str(b))
+elif (c > a and c > b):
+    print("largest number" +str(c))
