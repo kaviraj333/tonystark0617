42196cc2fd519e893a5565e0fde20132af9e6113
tonystark0617 <42027695+tonystark0617@users.noreply.github.com>
2019-01-20 23:53:40 +0530

    Update largestnumberfind.py

--- largestnumberfind.py
+++ largestnumberfind.py
@@ -2,7 +2,7 @@ N=int(input())
 if (1<=N<=100000):
     list1=[]
     for i in range (1,N+1,1):
-        c=int(input("enter : "))
+        c=int(input())
         list1.append(c)
     print(max(list1))
     
